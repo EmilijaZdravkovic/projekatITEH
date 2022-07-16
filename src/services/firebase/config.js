fedf94f0eefa71c4026654842a4182a058790490
EmilijaZdravkovic <emilijazdravkovic1998@gmail.com>
2022-07-16 08:22:48 +0200

    Dopunjena verzija aplikacije

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -2,6 +2,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+
 const firebaseConfig = {
     apiKey: "AIzaSyBaSIx9_uNBgytXCzaZOkQYcYad5sP7cS4",
     authDomain: "order-cf74b.firebaseapp.com",
@@ -10,6 +11,7 @@ const firebaseConfig = {
     messagingSenderId: "876863254203",
     appId: "1:876863254203:web:9fca3258c53ffed964039e"
 };
+
 
 // firebase initialization
 if(!firebase.apps.length) {
